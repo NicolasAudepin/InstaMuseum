14b24a0cd50670d25d67fc9a37c38f72bda0009b
NicolasAudepin <42012602+NicolasAudepin@users.noreply.github.com>
2021-07-04 23:25:23 +0200

    gh pages fonctionne
    is deployed with the random user api working

---
+++ instamuseum/src/App.js
@@ -0,0 +1,15 @@
+import FetchRandomUser from './components/FetchRandomUser';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FetchRandomUser/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
